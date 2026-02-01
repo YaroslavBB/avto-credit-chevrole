ecf4130c57795c05013f5b2a618c64f90a2b7fba
YaroslavBB <92986389+YaroslavBB@users.noreply.github.com>
2026-02-01 22:34:09 +0500

    добавил поддержку переменной в backend/config/plugins.ts.

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -2,4 +2,9 @@ export default () => ({
   "strapi-code-editor-custom-field": {
     enabled: true,
   },
+  "users-permissions": {
+    config: {
+      jwtSecret: process.env.JWT_SECRET,
+    },
+  },
 });
